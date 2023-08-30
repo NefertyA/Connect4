8d540c855f52383cde5eafab3217a0421001f7b7
NefertyA <143575620+NefertyA@users.noreply.github.com>
2023-08-29 23:42:45 -0400

    Create board.hpp

---
+++ board.hpp
@@ -0,0 +1,35 @@
+// Prevents redefinitions
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+using namespace std;
+
+class Board
+{
+
+// Data fields are protected so derived classes can access them
+protected:
+int size;
+char** board;
+
+// What a winning entry looks like for player 1 and 2
+string p1Win = "XXXX", p2Win = "OOOO";
+
+// All functions are public and abstract
+public:
+
+// Set and get functions for the board
+virtual void setBoard(int, int, int) = 0;
+virtual void getBoard() = 0;
+
+// Checks if the slot the player wants to play is valid
+virtual bool checkBoard(int, int) = 0;
+
+// Checks if a player has won or if the board has been filled
+virtual bool checkWin() = 0;
+virtual bool checkCompletion() = 0;
+
+};
+
+#endif
